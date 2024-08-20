009f5f1cb44f0e648d8bc8eb05f8f5882bc8c90f
unasir008 <158685990+unasir008@users.noreply.github.com>
2024-08-20 14:47:30 +0600

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_a12-user
-add_lunch_combo omni_a12-userdebug
-add_lunch_combo omni_a12-eng
+add_lunch_combo twrp_a12-user
+add_lunch_combo twrp_a12-userdebug
+add_lunch_combo twrp_a12-eng
